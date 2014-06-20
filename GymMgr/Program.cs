44c312c488113940c66737d717cdfbfb8712901e
khaikin <khaikinilia@gmail.com>
2014-06-20 16:34:21 +0300

    mutex

--- GymMgr/Program.cs
+++ GymMgr/Program.cs
@@ -14,6 +14,10 @@ namespace GymMgr
         [STAThread]
         static void Main()
         {
+            if (SingleInstanceCode.SingleInstanceClass.CheckForOtherApp("70C27BD8-E02D-432D-BBE9-4CC7BDDB6100"))
+                return;
+
+          
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
